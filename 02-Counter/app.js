3bc2545ec589a88ef9b3d2bf6c21e541831995ca
siam <asadsiam588@gmail.com>
2024-04-23 11:27:06 +0600

    counter app final code- completed

---
+++ 02-Counter/app.js
@@ -0,0 +1,31 @@
+//set initial counter value
+let count = 0;
+
+//  select value and buttons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+//select button from querrySelectorAll with loop
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    console.log(e.currentTarget.classList);
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else {
+      count = 0;
+    }
+    if (count > 0) {
+      value.style.color = "green";
+    }
+    if (count < 0) {
+      value.style.color = "red";
+    }
+    if (count == 0) {
+      value.style.color = "#222";
+    }
+    value.textContent = count;
+  });
+});
