856db6dc372b33689440dbb5fba018559713e770
siam <asadsiam588@gmail.com>
2024-05-02 12:02:11 +0600

    dynamic time for giveaway ends is completed

--- 12-countdown/app.js
+++ 12-countdown/app.js
@@ -21,3 +21,24 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector(".giveaway");
+const deadline = document.querySelector(".deadline");
+const items = document.querySelectorAll(".deadline-format h4");
+
+//give the current date
+//set our own date
+let futureDate = new Date(2024, 4, 23, 11, 30, 0);
+// console.log(futureDate);
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minute = futureDate.getMinutes();
+
+let month = futureDate.getMonth();
+month = months[month];
+
+const days = weekdays[futureDate.getDay()];
+
+const date = futureDate.getDate();
+
+giveaway.textContent = `giveaway ends on ${days}, ${date} ${month} ${year} at ${hours}:${minute}am`;
